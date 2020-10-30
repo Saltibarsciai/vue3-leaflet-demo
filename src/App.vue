fb986e31fb35ec8207c8203e8175201bfd890aa0
DominykasRakunas <roderik@jetcamp.com>
2020-10-30 22:26:36 +0200

    0.2.0

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-    <div style="height: 50vh; width: 50vw;">
-        <l-map
-                v-model="zoom"
-                v-model:zoom="zoom"
-                :center="[47.41322, -1.219482]"
-                @move="log('move')"
-        >
-            <l-tile-layer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            ></l-tile-layer>
-            <l-marker
-                    ref="marker"
-                    :lat-lng="[0, 0]"
-                    :icon="iconOne"
-                    draggable
-                    @moveend="log('moveend')"
-            ></l-marker>
-        </l-map>
+    <div>
+        <div style="height: 50vh; width: 50vw;">
+            <l-map
+                    v-model="zoom"
+                    v-model:zoom="zoom"
+                    :center="[47.41322, -1.219482]"
+                    ref="map"
+            >
+                <l-tile-layer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                ></l-tile-layer>
+                <l-marker
+                        ref="marker"
+                        :lat-lng="[0, 0]"
+                        :icon="iconOne"
+                >
+                    <l-popup>
+                        hello
+                    </l-popup>
+                </l-marker>
+                <l-marker
+                        ref="marker2"
+                        :lat-lng="[20, 4]"
+                >
+                    <l-popup>
+                        hellooo
+                    </l-popup>
+                </l-marker>
+            </l-map>
+        </div>
+        <button @click="changeIcon">
+            change icon
+        </button>
     </div>
-  <button @click="changeIcon">
-    change icon
-  </button>
 </template>
 <script>
-    import {LMap, LTileLayer, LMarker} from "@vue-leaflet/vue-leaflet";
+    import {LMap, LTileLayer, LMarker, LPopup} from "@vue-leaflet/vue-leaflet";
     import L from "leaflet"
     import "leaflet/dist/leaflet.css";
 
@@ -32,30 +44,110 @@
             LMap,
             LTileLayer,
             LMarker,
+            LPopup
         },
         data() {
             return {
                 zoom: 2,
+                map: [],
+                marker: []
             };
         },
+        mounted() {
+            // this.testVersion012();
+            // this.testVersion020();
+        },
         methods: {
-            log(a) {
-                console.log(a);
+            testVersion012() {
+                //have to double $nextTick to get this.$refs.map.mapObject
+                this.$nextTick(() => {
+                    this.$nextTick(() => {
+                        //bind mapObject ref to data object
+                        this.map = this.$refs.map.mapObject;
+                        //show map ref named "map"
+                        console.log('map ref - ', this.$refs.map);
+                        //show mapObject
+                        console.log('mapObject of map ref - ', this.$refs.map.mapObject);
+                        //remove ability to zoom when scrolling from map
+                        this.map.scrollWheelZoom.disable();
+                        //remove default zoom controls in order to create vustom ones
+                        this.map.zoomControl.remove();
+                        //add scale to map
+                        L.control.scale().addTo(this.map);
+                        //add custom control to the right side
+                        L.control.zoom({position: 'topright'}).addTo(this.map);
+
+                        this.map.on('popupopen', () => {
+                            console.log('event popup open')
+                        });
+                        this.map.on('popupclose', () => {
+                            console.log('event popup close')
+                        });
+                        this.map.on('dragend', () => {
+                            console.log('get bound when map moved', this.map.getBounds());
+                        });
+                        this.map.on('zoomend', () => {
+                            console.log('get bound when map zoomed', this.map.getBounds());
+                        });
+                    });
+                });
             },
-          changeIcon() {
-              this.$refs['marker'].icon.options.iconUrl = 'g.jpg'
-          }
+            testVersion020(){
+                //have to double $nextTick to get this.$refs.map.mapObject
+                this.$nextTick(() => {
+                    this.$nextTick(() => {
+                        //bind mapObject ref to data object
+                        this.map = this.$refs.map.leafletObject;
+                        //show map ref named "map"
+                        console.log('map ref - ', this.$refs.map);
+                        //show mapObject
+                        console.log('mapObject of map ref - ', this.$refs.map.leafletObject);
+                        //remove ability to zoom when scrolling from map
+                        this.map.scrollWheelZoom.disable();
+                        //remove default zoom controls in order to create vustom ones
+                        this.map.zoomControl.remove();
+                        //add scale to map
+                        L.control.scale().addTo(this.map);
+                        //add custom control to the right side
+                        L.control.zoom({position: 'topright'}).addTo(this.map);
+
+                        this.map.on('popupopen', () => {
+                            console.log('event popup open')
+                        });
+                        this.map.on('popupclose', () => {
+                            console.log('event popup close')
+                        });
+                        this.map.on('dragend', () => {
+                            console.log('get bound when map moved', this.map.getBounds());
+                        });
+                        this.map.on('zoomend', () => {
+                            console.log('get bound when map zoomed', this.map.getBounds());
+                        });
+                    });
+                });
+            },
+            testVersion020Click(){
+                this.map = this.$refs.map.leafletObject;
+                this.map.scrollWheelZoom.disable();
+                //remove default zoom controls in order to create vustom ones
+                this.map.zoomControl.remove();
+                //add scale to map
+                L.control.scale().addTo(this.map);
+                //add custom control to the right side
+                L.control.zoom({position: 'topright'}).addTo(this.map);
+                console.log(this.$refs.map.leafletObject)
+            },
+            changeIcon() {
+                //how to change icon via refs?
+                console.log('change shrek icon to kitten', this.$refs['marker'])
+                this.$refs['marker'].icon.options.iconUrl = 'g.jpg';
+                this.testVersion020Click();
+            }
         },
         computed: {
             iconOne() {
                 return L.icon({
                     iconUrl: 'e.jpg',
-                    iconSize: [51, 58],
-                })
-            },
-            iconTwo() {
-                return L.icon({
-                    iconUrl: 'g.jpg',
                     iconSize: [51, 58],
                 })
             }
